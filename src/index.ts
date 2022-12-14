cbbcf87d00258203f83ffae75b97df2c56605258
Nguyen Lam <nguyenlam@MacBook-Pro-14.local>
2022-12-14 21:34:28 +0700

    update controller

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,9 @@ AppDataSource.initialize().then(async () => {
     app.use(bodyParser.json())
     app.use('/', routers)
     // start express server
-    app.listen(3000)
+    const PORT = process.env.PORT
+    app.listen(PORT)
 
-    console.log("Express server has started on port 3000")
+    console.log(`Express server has started on port ${PORT}`)
 
 }).catch(error => console.log(error))
